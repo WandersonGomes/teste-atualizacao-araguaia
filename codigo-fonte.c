1dd552f1c34321432b5dc0934c8ca495f4c8e067
WandersonGomes <dersom100@gmail.com>
2023-11-22 13:23:52 -0300

    atualizacao

--- codigo-fonte.c
+++ codigo-fonte.c
@@ -13,11 +13,9 @@ E-mail: dersom100@gmail.com
 char* criarString(int tamanho);
 void destruirString(char* string);
 
-void mostrarMensagem(LPCSTR mensagem);
-
 void fecharProcessos();
 void fazerDownloadExecutaveis();
-void copiarExecutaveisParaDiretorios();
+int copiarExecutaveisParaDiretorios();
 
 // --- PROGRAMA PRINCIPAL ---
 int main() {
@@ -38,6 +36,7 @@ void destruirString(char* string) {
 }
 
 void fecharProcessos() {
+    puts("\n--- FECHANDO PROCESSOS ---");
     char* programa = "taskkill /F /IM";
     char nome_processo[50];
     FILE* arquivo = fopen("processos.cfg", "r");
@@ -57,6 +56,7 @@ void fecharProcessos() {
 }
 
 void fazerDownloadExecutaveis() {
+    puts("\n--- FAZENDO DOWNLOAD DOS EXECUTAVEIS ---");
     FILE* arquivo_executaveis = fopen("executaveis.cfg", "r");
     FILE* arquivo_repositorio = fopen("repositorio.cfg", "r");
     char* programa = "curl -o";
@@ -85,32 +85,52 @@ void fazerDownloadExecutaveis() {
     }
 }
 
-void copiarExecutaveisParaDiretorios() {
-    FILE* arquivo_diretorios = fopen("diretorios.cfg", "r");
-    FILE* arquivo_executaveis = fopen("executaveis.cfg", "r");
+void prepararString(char* string) {
+    if (string[strlen(string) - 1] == '\n')
+        string[strlen(string) - 1] = '\0';
+}
 
+int copiarExecutaveisParaDiretorios() {
+    puts("\n--- FAZENDO COPIA PARA OS DIRETORIOS ---");
+    // define o comando de copia
     char* programa = "copy";
-    char diretorio[100];
-    char executavel[50];
 
-    if (arquivo_diretorios != NULL) {
-        while (fscanf(arquivo_diretorios, "%s", diretorio) != EOF) {
-            if (arquivo_executaveis != NULL) {
-                while (fscanf(arquivo_executaveis, "%s", executavel) != EOF) {
-                    char* comando = criarString(strlen(programa) + strlen(executavel) + strlen(diretorio) + 2); //espaco em branco
-                    sprintf(comando, "%s %s %s", programa, executavel, diretorio);
-                    system(comando);
-                    destruirString(comando);
-                }
-                
-                fclose(arquivo_executaveis);
-            } else {
-                puts("ERROR: Nao foi possivel abrir o arquivo executaveis.cfg");
-            }
+    // abre os arquivos
+    FILE* arquivo_diretorios = fopen("diretorios.cfg", "r");
+    if (arquivo_diretorios == NULL) {
+        puts("ERROR: Nao foi possivel abrir o arquivo diretorios.cfg");
+        return EXIT_FAILURE;
+    }
+
+    FILE* arquivo_executaveis = fopen("executaveis.cfg", "r");
+    if (arquivo_executaveis == NULL) {
+        puts("ERROR: Nao foi possivel abrir o arquivo executaveis.cfg");
+        fclose(arquivo_diretorios);
+        return EXIT_FAILURE;
+    }
+
+    // realiza o comando para cada executavel e diretorio 
+    char* diretorio = NULL;
+    unsigned int len = 0;
+    while (getline(&diretorio, &len, arquivo_diretorios) != -1) {
+        prepararString(diretorio);
+        printf("Diretorio: %s\n", diretorio);
+        
+        char* executavel = NULL;
+        len = 0;
+        while (getline(&executavel, &len, arquivo_executaveis) != -1) {
+            prepararString(executavel);
+            printf("\tExecutavel: %s\n", executavel);
+
+            char* comando = criarString(strlen(programa) + strlen(executavel) + strlen(diretorio) + 3); //2 espacos e o \n
+            sprintf(comando, "%s %s \"%s\\\"", programa, executavel, diretorio);
+            printf("\tComando: %s\n", comando);
+            system(comando);
         }
 
-        fclose(arquivo_diretorios);
-    } else {
-        puts("ERROR: Nao foi possivel abrir o arquivo diretorios.cfg");
+        fseek(arquivo_executaveis, 0, SEEK_SET);
     }
+
+    fclose(arquivo_executaveis);
+    fclose(arquivo_diretorios);
 }
